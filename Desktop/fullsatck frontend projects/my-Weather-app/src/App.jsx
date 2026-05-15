92c84608247d1bec9b93f4770686a5ac3cb29d1f
roshni selvam <roshni8145@gmail.com>
2026-05-15 17:27:39 +0530

    feat: added weather app source code

---
+++ Desktop/fullsatck frontend projects/my-Weather-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Weather from "./assets/Weather";
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <h2 className="title">Weather App </h2>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
