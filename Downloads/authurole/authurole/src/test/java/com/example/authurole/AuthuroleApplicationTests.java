e652ea8a627f8478cf32b5277317e4cd876e8c08
roshni selvam <roshni8145@gmail.com>
2026-05-27 16:02:51 +0530

    feat: Spring Boot JWT auth backend

---
+++ Downloads/authurole/authurole/src/test/java/com/example/authurole/AuthuroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authurole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
